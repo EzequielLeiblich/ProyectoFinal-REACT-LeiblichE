a7c75defdd955ed333f735a839a2354d7e13e940
Ezequiel Leiblich <ezequiel.leiblich@gmail.com>
2023-03-16 00:31:58 -0300

    Creando asyncMock - ItemList - Item

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Counter from './components/Counter/Counter';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Sidebar />
-      <br/>
-      <ItemListContainer greeting={'Bienvenidos a mi E-commerce'}/>
-      <Counter />  
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Sidebar />
+            <br/>
+            <ItemListContainer greeting={'Bienvenidos a mi E-commerce'}/>
+            <Counter />  
+        </div>
+    );
 }
 
 export default App;
